a66aec405e1974bda442b01bbd8e944bae07a556
ᴅᴀʀᴋ ʀᴇᴀᴘᴇʀ <117750943+Dark-Reaper6@users.noreply.github.com>
2022-11-12 17:01:39 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About(props) {
                 <img src="https://source.unsplash.com/1000x300?/laptops,programing" className="card-img-top" alt="..." />
                 <div className={`card-body bg-${props.mode}`} style={{color: props.txtfieldColor(), transition: ".7s"}}>
                     <h5 className="card-title">{props.title}</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text">My greetings to the visitors! :) . I am Saad and i developed this little react app which would give you some utilities regarding to your whatever text. I hope you liked it. Don't forget to try out the amazing color modes, you can trigger it from the palette in the navigation bar. And try refreshing this "about" page, each time you refresh it, a different image would appear here :D</p>
                     <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
